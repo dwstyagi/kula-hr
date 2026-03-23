b199f1fca14381ad48e755ff8151db1f6df2e997
Diwanshu Tyagi <dwstyagi@gmail.com>
2026-03-23 17:18:08 +0530

    fix: update email sender domain from kulahr.com to kula-hr.com
    Updated the Devise mailer sender configuration to use the correct domain name, changing from "no-reply@kulahr.com" to "noreply@kula-hr.com" for consistency with the application's branding.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "no-reply@kulahr.com"
+  config.mailer_sender = "noreply@kula-hr.com"
 
   # Configure the class responsible to send e-mails.
   config.mailer = "KulaDeviseMailer"
